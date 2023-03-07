1f6f7d705764cfb21ff8cb5505c7cdc73df21a19
divkovic <dragoslav.ivkovic@creen.io>
2023-03-07 12:18:28 +0100

    task: user alredy exist

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,20 +1,20 @@
-
 const User = require('../models/userModel');
 
 const createUser = async (req, res) => {
-  const email = req.body.email;
-  const findUser = await User.findOne({ email: email });
+  try {
+    const email = req.body.email;
+    const findUser = await User.findOne({ email: email });
 
-  if (!findUser) {
-    const newUser = await User.create(req.body);
+    if (!findUser) {
+      const newUser = await User.create(req.body);
 
-    res.json(newUser);
-  } else {
-    res.json({
-      msg: 'User already exists',
-      success: false,
-    });
+      res.json(newUser);
+    } else {
+      throw new Error('User already exists');
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-};
+}
 
 module.exports = { createUser };
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const { notFound, errorHanlder } = require("./middleware/errorHandler");
 const app = express();
+require("dotenv").config({ path: ".env" });
 const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoutes")
 connectDB();
@@ -9,6 +11,9 @@ app.use(express.json()); // middleware za parsiranje JSON podataka
 app.use(express.urlencoded({ extended: true })); // middleware za parsiranje URL kodiranih podataka
 // Dodajemo authRouter za rute
 app.use("/api/user", authRouter);
+
+app.use(notFound);
+app.use(errorHanlder)
 
 app.listen(PORT, () => {
     console.log(`server is running at port:${PORT}`)
